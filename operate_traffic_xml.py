a55090e57d3dad69f255d30fa6e28331efa8f7e2
Chelong <chelong821204@gmail.com>
2018-06-19 00:11:53 +0800

    modify, add fields avg speed

--- operate_traffic_xml.py
+++ operate_traffic_xml.py
@@ -21,8 +21,10 @@ with open('section_id_with_youbike_info.csv') as csvfile:
 # make list unique
 #section_id_list = list(set(section_id_list))
 
+file_name = '2018_06_18_14_31_49.xml'
+
 # load xml file
-tree = ET.ElementTree(file='GetVD.xml')
+tree = ET.ElementTree(file=file_name)
 
 # xml prefix of tag
 vdPrefix = "{http://www.iii.org.tw/dax/vd}"
@@ -38,9 +40,14 @@ for child_of_root in root:
     if child_of_root.tag == vdPrefix + "SectionDataSet":
         trafficInfoRoot = child_of_root
 
-header = ["youbike_stop_num", "lat", "lng", "road_section_id", "road_avg_speed"]
+header = ["youbike_stop_num", "lat", "lng", "road_section_id", "road_avg_speed", "time"]
 insertList = []
 insertList.append(header)
+
+# operate time formate
+b_time = file_name[0:10].replace("_", "-")
+s_time = file_name[11:19].replace("_", ":")
+time_formate = b_time + " " + s_time
 
 # itr section_id_list
 for idx, val in enumerate(section_id_list):
@@ -48,7 +55,7 @@ for idx, val in enumerate(section_id_list):
         section_id = sdata.find( vdPrefix + "SectionId").text
         if section_id == val:
             avg_spd = sdata.find( vdPrefix + "AvgSpd").text
-            insertList.append(['{0:04}'.format(int(youbike_stop_list[idx])), lat_list[idx], lng_list[idx], section_id, avg_spd])
+            insertList.append(['{0:04}'.format(int(youbike_stop_list[idx])), lat_list[idx], lng_list[idx], section_id, avg_spd, time_formate])
 
 # write file
 
