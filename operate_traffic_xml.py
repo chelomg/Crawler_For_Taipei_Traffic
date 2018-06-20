05bc37526ed82eb54874eac39b41edcfa0251a38
Chelong <chelong821204@gmail.com>
2018-06-21 01:00:23 +0800

    modify operate road info csv and add road info result

--- operate_traffic_xml.py
+++ operate_traffic_xml.py
@@ -5,6 +5,7 @@ except ImportError:
 
 from time import gmtime, strftime
 import csv
+import glob
 
 section_id_list = []
 youbike_stop_list = []
@@ -21,49 +22,58 @@ with open('section_id_with_youbike_info.csv') as csvfile:
 # make list unique
 #section_id_list = list(set(section_id_list))
 
-file_name = '2018_06_18_14_31_49.xml'
+date_zoom = ["2018_06_10", "2018_06_11", "2018_06_12", "2018_06_13", "2018_06_14", "2018_06_15", "2018_06_16", "2018_06_17", "2018_06_18"]
 
-# load xml file
-tree = ET.ElementTree(file=file_name)
+for date_zoom_item in date_zoom:
+    fileList = glob.glob("../row_data/" + date_zoom_item + "/*.xml")
 
-# xml prefix of tag
-vdPrefix = "{http://www.iii.org.tw/dax/vd}"
+    for file_i in fileList:
+        file_name = file_i
+        file_i = file_i.split('/')
 
-# find root
-root = tree.getroot()
+        FN = file_i[len(file_i) - 1]
+        D = file_i[len(file_i) - 2].replace("_", ":")
+        hour = '{0:02}'.format((int(FN[11:13]) + 8)%24) + ":00:00"
 
-# find roor traffic info child
-trafficInfoRoot = {}
+        # load xml file
+        tree = ET.ElementTree(file=file_name)
 
-# iter traffic info
-for child_of_root in root:
-    if child_of_root.tag == vdPrefix + "SectionDataSet":
-        trafficInfoRoot = child_of_root
+        # xml prefix of tag
+        vdPrefix = "{http://www.iii.org.tw/dax/vd}"
 
-header = ["youbike_stop_num", "lat", "lng", "road_section_id", "road_avg_speed", "time"]
-insertList = []
-insertList.append(header)
+        # find root
+        root = tree.getroot()
 
-# operate time formate
-b_time = file_name[0:10].replace("_", "-")
-s_time = file_name[11:19].replace("_", ":")
-time_formate = b_time + " " + s_time
+        # find roor traffic info child
+        trafficInfoRoot = {}
 
-# itr section_id_list
-for idx, val in enumerate(section_id_list):
-    for sdata in trafficInfoRoot.iter( vdPrefix + "SectionData"):
-        section_id = sdata.find( vdPrefix + "SectionId").text
-        if section_id == val:
-            avg_spd = sdata.find( vdPrefix + "AvgSpd").text
-            insertList.append(['{0:04}'.format(int(youbike_stop_list[idx])), lat_list[idx], lng_list[idx], section_id, avg_spd, time_formate])
+        # iter traffic info
+        for child_of_root in root:
+            if child_of_root.tag == vdPrefix + "SectionDataSet":
+                trafficInfoRoot = child_of_root
 
-# write file
+        header = ["youbike_stop_num", "lat", "lng", "road_section_id", "road_avg_speed", "time"]
+        insertList = []
+        insertList.append(header)
 
-fileName = strftime("%Y_%m_%d_%H_%M_%S", gmtime()) + "_roadinfo.csv"
-myFile = open(fileName, 'w')
+        # operate time formate
+        time_formate = D + " " + hour
 
-with myFile:
-    writer = csv.writer(myFile)
-    writer.writerows(insertList)
+        # itr section_id_list
+        for idx, val in enumerate(section_id_list):
+            for sdata in trafficInfoRoot.iter( vdPrefix + "SectionData"):
+                section_id = sdata.find( vdPrefix + "SectionId").text
+                if section_id == val:
+                    avg_spd = sdata.find( vdPrefix + "AvgSpd").text
+                    insertList.append(['{0:04}'.format(int(youbike_stop_list[idx])), lat_list[idx], lng_list[idx], section_id, avg_spd, time_formate])
+
+        # write file
+
+        fileName =  "result1/" + D + "_" + hour + "_roadinfo.csv"
+        myFile = open(fileName, 'w')
+
+        with myFile:
+            writer = csv.writer(myFile)
+            writer.writerows(insertList)
 
 print "Complete!"
