eea60859b3cfe248be5ec9030a811384a9bd0595
Chelong <chelong821204@gmail.com>
2018-06-21 02:01:33 +0800

    add merge youbike files python file and result

---
+++ result/merge_youbike_files.py
@@ -0,0 +1,13 @@
+import csv
+import glob
+
+fileList = glob.glob("*.csv")
+
+with open('youbike_result.csv' ,'w') as outFile:
+    fileWriter = csv.writer(outFile)
+    for idx, file_name in enumerate(fileList):
+        with open(file_name,'r') as inFile:
+            fileReader = csv.reader(inFile)
+            for child_idx, row in enumerate(fileReader):
+                if idx == 0 or (idx != 0 and child_idx != 0):
+                    fileWriter.writerow(row)
