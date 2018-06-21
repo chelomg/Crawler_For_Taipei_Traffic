331886e8e643ccf4b9d73bb6cd752553bf63845a
Chelong <chelong821204@gmail.com>
2018-06-21 15:48:04 +0800

    update time formate

---
+++ result1/roadinfo_result_datetime_filter.py
@@ -0,0 +1,14 @@
+import csv
+import glob
+
+with open('roadinfo_updated_result.csv' ,'w') as outFile:
+    fileWriter = csv.writer(outFile)
+    with open('roadinfo_result.csv','r') as inFile:
+        fileReader = csv.reader(inFile)
+        for idx, row in enumerate(fileReader):
+            if idx != 0:
+                time = row[len(row) - 1]
+                time_array = time.split( )
+                time_array[0] = time_array[0].replace(":", "-")
+                row[len(row) - 1] = time_array[0] + " " + time_array[1]
+            fileWriter.writerow(row)
